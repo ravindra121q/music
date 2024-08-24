99949f3185e6a79a62688a561b34ad404173ebed
Ravindra Kumar <react2@flyweis.technology>
2024-08-24 15:00:18 +0530

    updated backend

---
+++ MusicPlayer/Backend/server.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./Database/Db');
+require('dotenv').config();
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.use('/auth', require('./Routes/authRoutes'));
+app.use('/songs', require('./Routes/songsRoutes'));
+app.use('/playlist', require('./Routes/playlistRoutes'));
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    connectDB();
+})
