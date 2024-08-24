c3f3add41b54b145328fdb582a0480d14e914d9c
Ravindra Kumar <react2@flyweis.technology>
2024-08-24 15:14:42 +0530

    updated

--- MusicPlayer/Backend/server.js
+++ MusicPlayer/Backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 app.use('/auth', require('./Routes/authRoutes'));
 app.use('/songs', require('./Routes/songsRoutes'));
 app.use('/playlist', require('./Routes/playlistRoutes'));
