d119ea778d1ad378a75b3bbc7caeee33c66ed326
ravindra <react2@flyweis.technology>
2024-08-26 17:47:17 +0530

    updated

--- MusicPlayer/Backend/Database/Db.js
+++ MusicPlayer/Backend/Database/Db.js
@@ -1,27 +1,15 @@
-const connectDB = async () => {
-    const mongoUrl = process.env.MONGO_URL;
-    const maxRetries = 5;
-    let retries = 0;
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-    while (retries < maxRetries) {
-        try {
-            const connection = await mongoose.connect(mongoUrl, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-            });
-            console.log(`MongoDB Connected: ${connection.connection.host}`);
-            break; 
-        } catch (error) {
-            console.error(`MongoDB connection failed. Retry ${retries + 1}/${maxRetries}`);
-            retries += 1;
-            if (retries === maxRetries) {
-                console.error("Max retries reached. Exiting process.");
-                process.exit(1);
-            }
-            
-            await new Promise(res => setTimeout(res, 5000));
-        }
+const connectDB = async () => {
+    const mongoUrl=process.env.MONGO_URL
+    try {
+        const connection = await mongoose.connect(mongoUrl);
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
     }
-};
+}
+
+module.exports = connectDB;
